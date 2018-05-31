42fc4d840ba0e447f22d7517ff70810f8538d738
Karolína Jastřembská <st30646@student.upce.cz>
2018-05-31 18:51:20 +0200

    ok

---
+++ opakovani.py
@@ -0,0 +1,8 @@
+# nový kruh nyni nahradime pi
+polomer=5
+from math import pi
+print("Kruh o poloměru", polomer, "má obvod: ", 2*pi*polomer)
+print("Kruh o poloměru", polomer, "má obsah: ", pi*polomer*polomer)
+"""erghrughugh
+egkrgjhgj
+ergrekhhu"""
